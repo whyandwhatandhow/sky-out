0bc021b444cb73fbffb1944d2bdee8e4cf9e9dc6
ymd <yemingdidi@gmail.com>
2025-05-13 11:57:55 +0800

    2025/5/13 异常处理_用户已存在

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -25,5 +28,23 @@ public class GlobalExceptionHandler {
     }
 
 
+    /**
+     * 用户名重复
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'dddddd' for key 'employee.idx_username'
+        String massage=ex.getMessage();
+        if(massage.contains("Duplicate entry")){
+            String split[]=massage.split(" ");
+            String username=split[2];
+            String msg=username+ MessageConstant.USER_READY;
+            return Result.error(msg);
+        }else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 
 }
